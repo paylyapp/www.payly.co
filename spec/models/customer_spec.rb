599c4f4a419c4aefb812d432750965800fdfb65f
Tim Gleeson <tim.j.gleeson@gmail.com>
2013-07-25 07:24:30 +0100

    add specs

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Customer do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "allow a customer to submit their email address"
+
+  it "allow customer to receive an email from submitting their email address"
+
+  describe "email" do
+    it "should contain a link to pocket with a with a token"
+
+    it "should contian instructions that explain what to do"
+  end
+
+  describe "pocket" do
+    it "should contain a list of purchases the customer has made through payly"
+
+    it "should have a download link for digital downloads"
+
+    it "should allow the user to enter details about themselves"
+  end
+
+  describe "details" do
+    it "should encrypt all details about the customer"
+  end
+end

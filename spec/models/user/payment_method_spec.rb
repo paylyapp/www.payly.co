c6cdea3be124d8c848bb5ca8091179e57b2de407
Tim Gleeson <tim.j.gleeson@gmail.com>
2013-07-30 21:47:32 +0100

    added spec files

---
+++ spec/models/user/payment_method_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe User::PaymentMethod do
+  it "should allow the user to choose between pin payments and braintree"
+
+  describe "Pin Payments" do
+    it "should present the user with two inputs for the keys"
+  end
+
+  describe "Braintree" do
+    it "should present the user with inputs for the keys"
+  end
+end

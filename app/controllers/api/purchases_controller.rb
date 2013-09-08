7a369e3e774c50f4dd87fe2ed3f3ede93dc0f704
Tim Gleeson <tim.j.gleeson@gmail.com>
2013-09-08 22:51:01 +0100

    update api files

--- app/controllers/api/purchases_controller.rb
+++ app/controllers/api/purchases_controller.rb
@@ -1,9 +1,6 @@
 class Api::PurchasesController < ApplicationController
   before_filter :authenticate_user!
   prepend_before_filter :get_auth_token
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
-
-  include ActionView::Helpers::NumberHelper
 
   # /api/purchases/t_abcdefg123456.json
   def show

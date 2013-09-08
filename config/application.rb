fa71091e2a70abff16bf401f8c01565e9abbab57
Tim Gleeson <tim.j.gleeson@gmail.com>
2013-09-08 18:08:05 +0100

    add stripe js

--- config/application.rb
+++ config/application.rb
@@ -72,7 +72,7 @@ module Haystack
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
     config.assets.precompile += ['dashboard.js', 'home.js', 'devise.js']
-    config.assets.precompile += %w( transactions-pin-payments.js transactions-strip.js transactions-braintree.js )
+    config.assets.precompile += ['transactions-pin-payments.js', 'transactions-stripe.js', 'transactions-braintree.js']
 
     config.generators do |g|
       g.test_framework :rspec, fixture: true

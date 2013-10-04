ac9a5f29c2ec36e353007cb9ee274eb3fee47246
Tim Gleeson <tim.j.gleeson@gmail.com>
2013-10-04 15:36:54 +0100

    fix to testing environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,8 @@ Haystack::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
